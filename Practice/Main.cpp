b9c085d2f33f4434fc193a1c7d6be006a080a3bc
Sopiro <knwer782@naver.com>
2019-12-18 07:31:45 +0900

    Add project files.

---
+++ Practice/Main.cpp
@@ -0,0 +1,72 @@
+#include <Windows.h>
+
+#define log(x) std::cout << x << std::endl
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE : 
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	const auto CLASS_NAME = "practice";
+	const auto TITLE = "Window";
+
+	//Register the window class.
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx(&wc);
+
+	// Create the wiindow
+	HWND hWnd = CreateWindowEx(
+		0, CLASS_NAME,
+		TITLE,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+		nullptr, nullptr, hInstance, nullptr);
+
+	//Set window to show
+	ShowWindow(hWnd, SW_SHOW);
+
+	// Run the message loop.
+	MSG msg;
+	BOOL gResult;
+
+	while (gResult = (GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+		return -1;
+	else
+		return msg.wParam;
+
+	return 0;
+}
+
+void test(int a)
+{
+
+}
