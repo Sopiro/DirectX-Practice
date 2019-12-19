9c93e2d06f65a4a941575a74376c2a3e1bb754b9
Sopiro <knwer782@naver.com>
2019-12-19 11:38:41 +0900

    Create Window class

--- Practice/Main.cpp
+++ Practice/Main.cpp
@@ -1,72 +1,10 @@
 #include <Windows.h>
 #include <string>
-#include <sstream>
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(0);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "f");
-		}
-		break;
-	case WM_CHAR:
-	{
-		static std::string title;
-		title.push_back((char)wParam);
-		SetWindowText(hWnd, title.c_str());
-	}
-	break;
-	case WM_LBUTTONDOWN:
-	{
-		POINTS pt = MAKEPOINTS(lParam);
-		std::ostringstream oss;
-		oss << "(" << pt.x << ", " << pt.y << ")";
-		SetWindowText(hWnd, oss.str().c_str());
-	}
-	break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "Window.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
-	const auto CLASS_NAME = "practice";
-	const auto TITLE = "Window";
-
-	//Register the window class.
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = CLASS_NAME;
-	wc.hIconSm = nullptr;
-
-	RegisterClassEx(&wc);
-
-	// Create the wiindow
-	HWND hWnd = CreateWindowEx(
-		0, CLASS_NAME,
-		TITLE,
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
-		nullptr, nullptr, hInstance, nullptr);
-
-	//Set window to show
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(640, 480, "Test");
 
 	// Run the message loop.
 	MSG msg;
---
+++ Practice/Window.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Windows.h>
+
+class Window
+{
+private:
+	class WindowClass
+	{
+	public:
+		static const char* GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator = (const WindowClass&) = delete;
+		static constexpr const char* wndClassName = "Window";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const char* name) noexcept;
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator = (const Window&) = delete;
+private:
+	static LRESULT WINAPI HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	static LRESULT WINAPI HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+private:
+	int width;
+	int height;
+	HWND hWnd;
+};
